8fa53e76f7cd69c0fb039ac552987b1db2924f18
YuNandarMoe <yunandarmoe31@gmail.com>
2024-01-07 15:10:05 +0630

    add and display food

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -4,45 +4,75 @@
     <div class="wrapper">
         <h1>Manage Food</h1>
         <br />
-        <a href="#" class="btn btn-primary">Add Food</a>
+        <?php
+        if (isset($_SESSION['add'])) {
+            echo $_SESSION['add'];
+            unset($_SESSION['add']);
+        }
+        ?>
+        <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn btn-primary">Add Food</a>
         <table class="tbl-full">
             <thead>
                 <tr>
                     <th>S.N.</th>
-                    <th>Full Name</th>
-                    <th>Username</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Featured</th>
+                    <th>Active</th>
                     <th>Action</th>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mg Mg</td>
-                    <td>mgmg</td>
-                    <td>
-                        <a href="#" class="btn btn-secondary">Update</a>
-                        <a href="#" class="btn btn-danger">Delete</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Mg Mg</td>
-                    <td>mgmg</td>
-                    <td>
-                        <a href="#" class="btn btn-secondary">Update</a>
-                        <a href="#" class="btn btn-danger">Delete</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Mg Mg</td>
-                    <td>mgmg</td>
-                    <td>
-                        <a href="#" class="btn btn-secondary">Update</a>
-                        <a href="#" class="btn btn-danger">Delete</a>
-                    </td>
-                </tr>
-            </tbody>
+            <?php
+            $sql = "SELECT * FROM tbl_food";
+
+            $res = mysqli_query($conn, $sql);
+
+            $count = mysqli_num_rows($res);
+
+            $sn = 1;
+
+            if ($count > 0) {
+                while ($row = mysqli_fetch_assoc($res)) {
+                    $id = $row['id'];
+                    $title = $row['title'];
+                    $price = $row['price'];
+                    $image_name = $row['image_name'];
+                    $featured = $row['featured'];
+                    $active = $row['active'];
+
+            ?>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $sn++; ?></td>
+                            <td><?php echo $title ?></td>
+                            <td><?php echo $price ?></td>
+                            <td>
+                                <?php
+                                if ($image_name == "") {
+                                    echo "<div class='error'>Image not added</div>";
+                                } else {
+                                    ?>
+                                    <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name ?>" alt="" width="100px">
+                                    <?php
+                                }
+                                ?>
+                            </td>
+                            <td><?php echo $featured ?></td>
+                            <td><?php echo $active ?></td>
+                            <td>
+                                <a href="#" class="btn btn-secondary">Update</a>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+            <?php
+                }
+            } else {
+                echo "<tr><td colspan='7' class='danger'>Food not added yet</td></tr>";
+            }
+            ?>
+
         </table>
     </div>
 </div>
