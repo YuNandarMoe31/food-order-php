499cb7a8819d2d5273049b72a9180c52278b1dbd
YuNandarMoe <yunandarmoe31@gmail.com>
2024-01-14 11:28:44 +0630

    create summary dashboard on admin panel

--- admin/index.php
+++ admin/index.php
@@ -5,32 +5,63 @@
     <div class="wrapper">
         <h1>Dashboard</h1>
         <br>
-        
+
         <?php
-            if(isset($_SESSION['login'])) {
-                echo $_SESSION['login'];
-                unset($_SESSION['login']);
-            }
+        if (isset($_SESSION['login'])) {
+            echo $_SESSION['login'];
+            unset($_SESSION['login']);
+        }
         ?>
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php
+            $sql = "SELECT * FROM tbl_category";
+
+            $res = mysqli_query($conn, $sql);
+
+            $count = mysqli_num_rows($res);
+            ?>
+            <h1><?php echo $count ?></h1>
             <br />
             Categories
         </div>
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php
+            $sql2 = "SELECT * FROM tbl_food";
+
+            $res2 = mysqli_query($conn, $sql2);
+
+            $count2 = mysqli_num_rows($res2);
+            ?>
+            <h1><?php echo $count2 ?></h1>
             <br />
-            Categories
+            Foods
         </div>
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php
+            $sql3 = "SELECT * FROM tbl_order";
+
+            $res3 = mysqli_query($conn, $sql3);
+
+            $count3 = mysqli_num_rows($res3);
+            ?>
+            <h1><?php echo $count3 ?></h1>
             <br />
-            Categories
+            Total Orders
         </div>
         <div class="col-4 text-center">
-            <h1>5</h1>
+            <?php
+            $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered'";
+
+            $res4 = mysqli_query($conn, $sql4);
+
+            $row4 = mysqli_fetch_assoc($res4);
+
+            // get total revenue
+            $total_revenue = $row4['Total'];
+            ?>
+            <h1>$ <?php echo $total_revenue ?></h1>
             <br />
-            Categories
+            Revenue Generated
         </div>
         <div class="clearfix">
         </div>
